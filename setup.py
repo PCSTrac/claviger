edf157ef7adc91758e0290993eb6f45f7fda644c
Klathmon <gregbenner1@gmail.com>
2016-04-06 10:51:02 -0400

    cleanup some random files

--- setup.py
+++ setup.py
@@ -5,14 +5,12 @@ from setuptools import setup
 
 setup(
     name='claviger',
-    version='0.2.2.dev0',
-    description='Synchronizes remote SSH authorized_keys',
-    long_description="{0:s}\n{1:s}". format(
-                    open('README.rst').read(),
-                    open('CHANGES.rst').read()),
-    author='Bas Westerbaan',
-    author_email='bas@westerbaan.name',
-    url='http://github.com/bwesterb/claviger/',
+    version='0.5.0',
+    description='Manages users and SSH keys across multiple servers',
+    long_description="{0:s}". format(open('README.md').read()),
+    author='Gregory Benner',
+    author_email='gregory.benner@pcstrac.com',
+    url='http://github.com/PCSTrac/claviger/',
     packages=['claviger', 'claviger.tests'],
     package_dir={'claviger': 'src'},
     package_data={'claviger': [
